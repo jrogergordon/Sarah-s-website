14ea91c93147bc000a02ac2539456ea92740fc89
Jacob <jrogergordon@gmail.com>
2023-11-04 23:58:22 -0400

    images, github

---
+++ js-index-thumbs.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     let one = document.getElementById('box#1');
+//     console.log(one)
+//     one.addEventListener('click', () => {
+//         if (one.classList.contains('darkened')) {
+//             one.classList.remove('darkened')
+//         } else {
+//             one.classList.add('darkened')
+//         }
+//     });
+
+
+// })
--- js-index.js
+++ js-index.js
@@ -1,84 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
-    // window.addEventListener("keydown", function (e) {
-    //     if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(e.code) > -1) {
-    //         e.preventDefault();
-    //     }
-    // }, false);
-
-
-    // function createMatrix(x, y) {
-    //     function topWallCheck(node) {
-    //         const topWall = [[4, 8], [9, 18], [4, 18], [9, 8]];
-    //         for (let i = 0; i < topWall.length; i++) {
-    //             if (topWall[i][0] === node[0] && topWall[i][1] === node[1]) {
-    //                 return true;
-    //             }
-    //         }
-    //         return false;
-    //     }
-    //     function wallCheck(node) {
-    //         const cols = [[10, 8], [9, 8], [10, 9],
-    //         [4, 9], [5, 8], [4, 8],
-    //         [10, 18], [10, 17], [9, 18],
-    //         [4, 18], [4, 17], [5, 18]]
-    //         for (let i = 0; i < cols.length; i++) {
-    //             if (cols[i][0] === node[0] && cols[i][1] === node[1]) {
-    //                 return true;
-    //             }
-    //         }
-    //         return false;
-    //     }
-    //     let topColumn = [[4, 8], [9, 18], [4, 18], [9, 8]];
-    //     let bottomColumn = [[10, 8], [10, 9], [4, 9], [5, 8],
-    //     [10, 18], [10, 17], [4, 17], [5, 18]]
-    //     let arr2d = new Array(x)
-    //     for (let i = 0; i < x; i++) {
-    //         arr2d[i] = new Array(y);
-    //         for (let j = 0; j < y; j++) {
-    //             if (i === 0 || j === 0 || i === x - 1 || j === y - 1) {
-    //                 arr2d[i][j] = 0
-    //             } else if (i < 9 && i > 5 && j < 17 && j > 9) {
-    //                 arr2d[i][j] = 3
-    //             } else if (topWallCheck([i, j])) {
-    //                 arr2d[i][j] = 8
-    //             } else if (wallCheck([i, j])) {
-    //                 arr2d[i][j] = 7
-    //             } else {
-    //                 arr2d[i][j] = 1
-    //             }
-    //             // if (wallCheck([i, j])) {
-    //             //     arr2d[row][col] = 6;
-    //             // }
-    //             // if (topWallCheck([i, j])) {
-    //             //     arr2d[row][col] = 7;
-    //             // }
-    //         }
-    //     }
-    //     return arr2d;
-    // }
-
-    // const matrix = createMatrix(15, 27);
-    // const grid = new GridSystem(matrix);
-
-    // let instructions = document.querySelector('.instructions');
-    // let close_instructions = document.querySelector('.close-button');
-    // let constInst = document.querySelector('.in-game-inst')
-    // let music = document.querySelector('.audio-example')
-
-    // close_instructions.addEventListener('click', () => {
-    //     instructions.classList.add('invis');
-    //     constInst.classList.add('present');
-    //     music.volume = 0.07;
-    //     music.play();
-    //     let playingMusic = true;
-    // });
     let boxOne = document.querySelector('.box_1');
     let boxTwo = document.querySelector('.box_2');
     let boxThree = document.querySelector('.box_3');
     let boxFour = document.querySelector('.box_4');
     let boxFive = document.querySelector('.box_5');
+    let boxSix = document.querySelector('.box_6');
 
     let audioBoxTwo = document.querySelectorAll('.audio_box_two');
     let playBoxTwo = document.querySelectorAll('div.audio_box_two > audio.hidden');
@@ -90,10 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let playBoxFour = document.querySelectorAll('div.audio_box_four > audio.hidden');
     let audioBoxFive = document.querySelectorAll('.audio_box_five');
     let playBoxFive = document.querySelectorAll('div.audio_box_five > audio.hidden');
+    let audioBoxSix = document.querySelectorAll('.audio_box_six');
+    let playBoxSix = document.querySelectorAll('div.audio_box_six > audio.hidden');
     // document.querySelectorAll("div.note, div.alert");
 
     function clickedAudioKey(clickedKey, audioClips, audioBox) {
-        console.log(audioClips, audioBox )
         if (clickedKey.classList.contains('lightened')) {
             clickedKey.classList.remove('lightened');
             clickedKey.classList.add('key_hover')
@@ -102,6 +30,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 audioBox[i].classList.add('hidden');
             }
         } else {
+            let href = document.getElementById("view_audio");
+            let href2 = href.getAttributeNode("href");
+            console.log(href2);
+            href.setAttribute("href", "#");
+            console.log(href2);
             clickedKey.classList.add('lightened');
             clickedKey.classList.remove('key_hover');
             for(let i = 0; i < audioClips.length; i++) {
@@ -130,7 +63,223 @@ document.addEventListener("DOMContentLoaded", () => {
 
     boxFive.addEventListener('click', () => {
         clickedAudioKey(boxFive, audioBoxFive, playBoxFive)
-    }); 
+    });
 
+    boxSix.addEventListener('click', () => {
+        clickedAudioKey(boxSix, audioBoxSix, playBoxSix)
+    });
+    
+    
+    function clickedThumb(thumbCell, thumbAudio) {
+        if (thumbCell.classList.contains('darkened')) {
+            thumbAudio.classList.remove('hidden')
+            thumbCell.classList.remove('darkened')
+        } else {
+            thumbAudio.classList.add('hidden')
+            thumbCell.classList.add('darkened')
+        }
+    }
+
+
+    let one = document.getElementById('box_1');
+    let oneAudio = document.querySelector('div#box_1 > audio.hidden');
+    one.addEventListener('click', () => {
+        clickedThumb(one, oneAudio)
+    });
+
+    let two = document.getElementById('box_2');
+    let twoAudio = document.querySelector('div#box_2 > audio.hidden');
+    two.addEventListener('click', () => {
+        clickedThumb(two, twoAudio)
+    });
+
+    let three = document.getElementById('box_3');
+    let threeAudio = document.querySelector('div#box_3 > audio.hidden');
+    three.addEventListener('click', () => {
+        clickedThumb(three, threeAudio)
+    });
+
+    let four = document.getElementById('box_4');
+    let fourAudio = document.querySelector('div#box_4 > audio.hidden');
+    four.addEventListener('click', () => {
+        clickedThumb(four, fourAudio)
+    });
+
+    let five = document.getElementById('box_5');
+    let fiveAudio = document.querySelector('div#box_5 > audio.hidden');
+    five.addEventListener('click', () => {
+        clickedThumb(five, fiveAudio)
+    });
+
+    let six = document.getElementById('box_6');
+    let sixAudio = document.querySelector('div#box_6 > audio.hidden');
+    six.addEventListener('click', () => {
+        clickedThumb(six, sixAudio)
+    });
+
+    let seven = document.getElementById('box_7');
+    let sevenAudio = document.querySelector('div#box_7 > audio.hidden');
+    seven.addEventListener('click', () => {
+        clickedThumb(seven, sevenAudio)
+    });
+
+    let eight = document.getElementById('box_8');
+    let eightAudio = document.querySelector('div#box_8 > audio.hidden');
+    eight.addEventListener('click', () => {
+        clickedThumb(eight, eightAudio)
+    });
+
+    let nine = document.getElementById('box_9');
+    let nineAudio = document.querySelector('div#box_9 > audio.hidden');
+    nine.addEventListener('click', () => {
+        clickedThumb(nine, nineAudio)
+    });
+
+    let ten = document.getElementById('box_10');
+    let tenAudio = document.querySelector('div#box_10 > audio.hidden');
+    ten.addEventListener('click', () => {
+        clickedThumb(ten, tenAudio)
+    });
+
+    let eleven = document.getElementById('box_11');
+    let elevenAudio = document.querySelector('div#box_11 > audio.hidden');
+    eleven.addEventListener('click', () => {
+        clickedThumb(eleven, elevenAudio)
+    });
+
+    let twelve = document.getElementById('box_12');
+    let twelveAudio = document.querySelector('div#box_12 > audio.hidden');
+    twelve.addEventListener('click', () => {
+        clickedThumb(twelve, twelveAudio)
+    });
+
+    let thirteen = document.getElementById('box_13');
+    let thirteenAudio = document.querySelector('div#box_13 > audio.hidden');
+    thirteen.addEventListener('click', () => {
+        clickedThumb(thirteen, thirteenAudio)
+    });
+
+    let fourteen = document.getElementById('box_14');
+    let fourteenAudio = document.querySelector('div#box_14 > audio.hidden');
+    fourteen.addEventListener('click', () => {
+        clickedThumb(fourteen, fourteenAudio)
+    });
+
+    let fifteen = document.getElementById('box_15');
+    let fifteenAudio = document.querySelector('div#box_15 > audio.hidden');
+    fifteen.addEventListener('click', () => {
+        clickedThumb(fifteen, fifteenAudio)
+    });
+
+    let sixteen = document.getElementById('box_16');
+    let sixteenAudio = document.querySelector('div#box_16 > audio.hidden');
+    sixteen.addEventListener('click', () => {
+        clickedThumb(sixteen, sixteenAudio)
+    });
+
+    let seventeen = document.getElementById('box_17');
+    let seventeenAudio = document.querySelector('div#box_17 > audio.hidden');
+    seventeen.addEventListener('click', () => {
+        clickedThumb(seventeen, seventeenAudio)
+    });
+
+    let eighteen = document.getElementById('box_18');
+    let eighteenAudio = document.querySelector('div#box_18 > audio.hidden');
+    eighteen.addEventListener('click', () => {
+        clickedThumb(eighteen, eighteenAudio)
+    });
+
+    let nineteen = document.getElementById('box_19');
+    let nineteenAudio = document.querySelector('div#box_19 > audio.hidden');
+    nineteen.addEventListener('click', () => {
+        clickedThumb(nineteen, nineteenAudio)
+    });
+
+    let twenty = document.getElementById('box_20');
+    let twentyAudio = document.querySelector('div#box_20 > audio.hidden');
+    twenty.addEventListener('click', () => {
+        clickedThumb(twenty, twentyAudio)
+    });
+
+    let twentyone = document.getElementById('box_21');
+    let twentyoneAudio = document.querySelector('div#box_21 > audio.hidden');
+    twentyone.addEventListener('click', () => {
+        clickedThumb(twentyone, twentyoneAudio)
+    });
+
+    let twentytwo = document.getElementById('box_22');
+    let twentytwoAudio = document.querySelector('div#box_22 > audio.hidden');
+    twentytwo.addEventListener('click', () => {
+        clickedThumb(twentytwo, twentytwoAudio)
+    });
+
+    let twentythree = document.getElementById('box_23');
+    let twentythreeAudio = document.querySelector('div#box_23 > audio.hidden');
+    twentythree.addEventListener('click', () => {
+        clickedThumb(twentythree, twentythreeAudio)
+    });
+
+    let twentyfour = document.getElementById('box_24');
+    let twentyfourAudio = document.querySelector('div#box_24 > audio.hidden');
+    twentyfour.addEventListener('click', () => {
+        clickedThumb(twentyfour, twentyfourAudio)
+    });
+
+    let twentyfive = document.getElementById('box_25');
+    let twentyfiveAudio = document.querySelector('div#box_25 > audio.hidden');
+    twentyfive.addEventListener('click', () => {
+        clickedThumb(twentyfive, twentyfiveAudio)
+    });
+    
+    let twentysix = document.getElementById('box_26');
+    let twentysixAudio = document.querySelector('div#box_26 > audio.hidden');
+    twentysix.addEventListener('click', () => {
+        clickedThumb(twentysix, twentysixAudio)
+    });
+
+    let twentyseven = document.getElementById('box_27');
+    let twentysevenAudio = document.querySelector('div#box_27 > audio.hidden');
+    twentyseven.addEventListener('click', () => {
+        clickedThumb(twentyseven, twentysevenAudio)
+    });
+
+    let twentyeight = document.getElementById('box_28');
+    let twentyeightAudio = document.querySelector('div#box_28 > audio.hidden');
+    twentyeight.addEventListener('click', () => {
+        clickedThumb(twentyeight, twentyeightAudio)
+    });
+
+    let twentynine = document.getElementById('box_29');
+    let twentynineAudio = document.querySelector('div#box_29 > audio.hidden');
+    twentynine.addEventListener('click', () => {
+        clickedThumb(twentynine, twentynineAudio)
+    });
+
+    let thirty = document.getElementById('box_30');
+    let thirtyAudio = document.querySelector('div#box_30 > audio.hidden');
+    thirty.addEventListener('click', () => {
+        clickedThumb(thirty, thirtyAudio)
+    });
+
+    let thirtyone = document.getElementById('box_31');
+    let thirtyoneAudio = document.querySelector('div#box_31 > audio.hidden');
+    thirtyone.addEventListener('click', () => {
+        clickedThumb(thirtyone, thirtyoneAudio)
+    });
+
+    let thirtytwo = document.getElementById('box_32');
+    let thirtytwoAudio = document.querySelector('div#box_32 > audio.hidden');
+    thirtytwo.addEventListener('click', () => {
+        clickedThumb(thirtytwo, thirtytwoAudio)
+    });
+
+    let thirtythree = document.getElementById('box_33');
+    let thirtythreeAudio = document.querySelector('div#box_33 > audio.hidden');
+    thirtythree.addEventListener('click', () => {
+        clickedThumb(thirtythree, thirtythreeAudio)
+    });
+
+
+    
 
 })
